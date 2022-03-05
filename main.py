c1ab228e620fd22db26d31680146e3422f128194
Jacob Pembroke-Burn <jacobpb13@live.co.uk>
2022-03-05 11:26:56 +0000

    most recent file changes, attempted to rebase

--- main.py
+++ main.py
@@ -11,17 +11,15 @@ import json
 import spotipy
 import spotipy.oauth2 as oauth2
 from spotipy.oauth2 import SpotifyClientCredentials
+import details
+from details import account_name,client_id,client_secret
 
 credentials = None
 scopes=['https://www.googleapis.com/auth/youtube.readonly']
 
-client_id='4f1eb090c2ba4ef9b1aaed2f98e748ce'
-client_secret='dc2ba2db68224eab98ab51aedc89f454'
-
-#sscope = "user-library-read"
-
-#sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=sscope))
-
+client_id= details.client_id
+client_secret= details.client_secret
+account = details.account_name
 
 
 
@@ -29,8 +27,8 @@ client_secret='dc2ba2db68224eab98ab51aedc89f454'
 def get_spotify_token():
     """ Generate the token. Please respect these credentials :) """
     spotify_credentials = oauth2.SpotifyClientCredentials(
-        client_id='4f1eb090c2ba4ef9b1aaed2f98e748ce',
-        client_secret='dc2ba2db68224eab98ab51aedc89f454')
+        client_id,
+        client_secret)
     spot_token = spotify_credentials.get_access_token()
     return spot_token
 
@@ -75,28 +73,6 @@ def youtube_auth(credentials):
     response = request.execute()
     return response
 
-            
-
-# def request_response():
-            
-#     youtube = build('youtube', 'v3', credentials=credentials)
-
-#     request = youtube.playlistItems().list(
-#         part='status, contentDetails',
-#         playlistId= 'PLaJd4NqiJg0gmkgnS7KJA8VV24UGg8hIs',
-#         maxResults = 50                         
-#     )
-#     response = request.execute()
-#     return response
-    # request = youtube.playlistItems().list(
-    #     part='snippet',
-    #     playlistId= 'RDLUP0NjzDLPY',
-    #     maxResults = 50                         
-    # )
-    
-    #print(response)
-
-
 
 def yt_song_info(list):
   
@@ -117,6 +93,7 @@ def yt_song_info(list):
     
 
 def create_spotify_playlist():
+    print("making playlist")
     request_body = json.dumps(
         {
             "name": "YouTube Playlist",
@@ -125,22 +102,25 @@ def create_spotify_playlist():
         }
     )
     spotify_request  = "https://api.spotify.com/v1/users/{}/playlists".format(
-        client_id)
+        account_name) 
+    
     spotify_response = requests.post(
         spotify_request,
         data = request_body,
         headers = {"Content-Type": "application/json",
-            "Authorization": "Bearer BQB9wDfMnKuYV3gjL2I9LdKf86yHlBy4DTItXk3V4d9jU9r44nIptHLd4X_yWSvz_XZ0bo2wZ1bQV9GUMt4iu8hzt0-iS7C0cU61UvHCwQp443AJ836-_B0JmdSEfR-6fzFmPn232twFx-iEwg0NoKTJv2NBEgT9cuOaZG7zw65Ft4nekPXuKLoztQ3540BNi2B86a-z2CiZind-KqZbtAjgUrPzoFpn-Ub_c-xginZ9bnydQA"}
+            "Authorization": "Bearer ###insert token###"}
     )
-    #print(spotify_response)
-    return spotify_response
+    
+    spotify_response = spotify_response.json()
+    print(spotify_response)
+    #return spotify_response["id"]
 
 def spotify_urls(track,artist):
     url_query = "https://api.spotify.com/v1/search?q={}%2C{}&type=track,artist".format(track,artist)
     response = requests.get(url_query,
         headers={
             "Content-Type": "application/json",
-            "Authorization": "Bearer BQB9wDfMnKuYV3gjL2I9LdKf86yHlBy4DTItXk3V4d9jU9r44nIptHLd4X_yWSvz_XZ0bo2wZ1bQV9GUMt4iu8hzt0-iS7C0cU61UvHCwQp443AJ836-_B0JmdSEfR-6fzFmPn232twFx-iEwg0NoKTJv2NBEgT9cuOaZG7zw65Ft4nekPXuKLoztQ3540BNi2B86a-z2CiZind-KqZbtAjgUrPzoFpn-Ub_c-xginZ9bnydQA"
+            "Authorization": "Bearer ###insert token###"
         }
     )
     response_json = response.json()
@@ -152,6 +132,7 @@ def spotify_urls(track,artist):
     return url
 
 def add_song_to_spotify_playlist(playlist_id,urls):
+    print("adding song to playlist")
     request_data = json.dumps(urls)
   
     query = "https://api.spotify.com/v1/playlists/{}/tracks".format(
@@ -162,11 +143,11 @@ def add_song_to_spotify_playlist(playlist_id,urls):
         data=request_data,
         headers={
             "Content-Type": "application/json",
-            "Authorization": "Bearer BQB9wDfMnKuYV3gjL2I9LdKf86yHlBy4DTItXk3V4d9jU9r44nIptHLd4X_yWSvz_XZ0bo2wZ1bQV9GUMt4iu8hzt0-iS7C0cU61UvHCwQp443AJ836-_B0JmdSEfR-6fzFmPn232twFx-iEwg0NoKTJv2NBEgT9cuOaZG7zw65Ft4nekPXuKLoztQ3540BNi2B86a-z2CiZind-KqZbtAjgUrPzoFpn-Ub_c-xginZ9bnydQA"
+            "Authorization": "Bearer ###insert token###"
         }
     )
   
-    return "Song added"
+    return "successfully added"
 
 
 stoken = get_spotify_token()
